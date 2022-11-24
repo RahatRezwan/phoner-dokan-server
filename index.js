604ed3753bf5bcc0ed7c79aa3724f42ad368ff9f
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-24 14:21:25 +0600

    create mongodb client

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -11,6 +12,17 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
    res.send("Phoner Dokan server is running");
+});
+
+/* Mongodb client */
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@phonerdokan.daagdyy.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   serverApi: ServerApiVersion.v1,
 });
 
 app.listen(port, () => {
